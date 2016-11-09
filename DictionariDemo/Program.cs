45d86e91e31e3e302dbb4602cfc7693c3bbdb94b
angelaky <anna@velichkov.net>
2016-11-09 11:12:55 +0200

    test

---
+++ DictionariDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionariDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyValuePair<int, int> ids = new KeyValuePair<int, int>(5,6);
+            Console.WriteLine(ids.Key);
+
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            students.Add(5, "Eli");
+            students.Add(6, "Gosho");
+            students.Add(7, "Stamat");
+
+            Console.WriteLine(students[5]);
+
+            foreach (var item in students)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("Key : {0} ---, Value : {1} ----",item.Key,item.Value);
+            }
+
+            if (students.ContainsKey(27))
+            {
+                Console.WriteLine(students[27]);
+            }
+
+            
+
+        }
+    }
+}
---
+++ WordsCount/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            Console.WriteLine("Enter text: ");
+            string line = Console.ReadLine();
+            string[] inputText = line.Split(new Char[] { ',', '\n', ' ', ';', ':' , '?', '!','.'}, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var item in inputText)
+            {
+                if (!words.ContainsKey(item))
+                {
+                    words.Add(item, 1);
+                }
+                else
+                {
+                    words[item]++;
+                } 
+            }
+
+            foreach (var item in words)
+            {
+                
+                Console.WriteLine("Key : {0}, Value : {1}", item.Key, item.Value);
+            }
+
+        }
+    }
+}
