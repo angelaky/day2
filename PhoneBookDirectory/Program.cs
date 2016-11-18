761e31f53f95ce7733d680eb57447ca2aeb36f8b
angelaky <anna@velichkov.net>
2016-11-18 17:57:42 +0200

    labala

---
+++ PhoneBookDirectory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
